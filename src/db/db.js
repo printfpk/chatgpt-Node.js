cf8ccf1eb7b9af52763ccd5da889e389f0ea921b
printf <printfpk@gmail.com>
2025-10-14 20:19:06 +0530

    basic setup done

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to MongoDB")
+    }
+    catch(err){
+        console.log("Error connecting to MongoDB", err)
+    }
+}
+
+module.exports = connectDB;
