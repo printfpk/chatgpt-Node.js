eacd05ce49e2ad7049b883dc1c9680ce95bef0f0
printf <printfpk@gmail.com>
2025-10-15 01:24:21 +0530

    authentication and chat api setup

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,22 @@
+const chatModel = require("../models/chat.model");
+
+async function createChat(req, res) {
+    const {title} = req.body;
+    const user = req.user;
+
+    const chat = await chatModel.create({title, user: user._id});
+
+    res.status(201).json({
+        message: "Chat created successfully",
+        chat:{
+            _id: chat._id,
+            title: chat.title,
+            lastActivity: chat.lastActivity,
+            timestamp: chat.timestamp
+        }
+    })
+}
+
+module.exports = {
+    createChat
+}
