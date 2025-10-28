80a2b1344bcfca438e83a5539a7e28cd12b4800a
printf <printfpk@gmail.com>
2025-10-28 19:27:53 +0530

    correction 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
-app.get("*", (req, res) => {
+app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 module.exports = app;
