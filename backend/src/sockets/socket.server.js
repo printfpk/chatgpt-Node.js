189e368dcc7c4b13ea61de585aa688f177f475c5
printf <printfpk@gmail.com>
2025-11-03 22:00:02 +0530

    fixing socket.io

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -9,7 +9,7 @@ const { createMemory, queryMemory } = require("../services/vector.service");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://chatgpt-node-js-43zk.onrender.com",
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
     }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
         console.log(form);
 
-        axios.post("https://cohort-1-project-chat-gpt.onrender.com/api/auth/login", {
+        axios.post("https://chatgpt-node-js-43zk.onrender.com/api/auth/login", {
             email: form.email,
             password: form.password
         },
