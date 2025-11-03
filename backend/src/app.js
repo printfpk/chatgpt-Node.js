feae0717ee58ed3b548fee5f860b10dec7f2d10b
printf <printfpk@gmail.com>
2025-11-03 19:43:23 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,8 @@ const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require('./routes/chat.routes');
 
 /*using middleware */
-app.use(cors(
-    {
-        origin: 'http://localhost:5173',
-        credentials: true
-    }
-));  
+app.use(cors({ origin: ['http://localhost:5173', 'https://chatgpt-node-js-43zk.onrender.com'], credentials: true }));
+
 
 app.use(express.json());
 app.use(cookieParser());
